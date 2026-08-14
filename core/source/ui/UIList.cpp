ffc283c4451fa4a228b5c12ec122bbf986788b95
Leo Roberts <leo@lfs.net>
2026-08-14 21:09:22 +0100

    Support list items stretching to fill space

--- core/source/ui/UIList.cpp
+++ core/source/ui/UIList.cpp
@@ -3,24 +3,59 @@
 void UIList::updateBounds(Rectangle parentBounds) {
 	UINode::updateBounds(parentBounds);
 
-	float currentPos = 0.f;
-
 	auto paddedBounds = getAbsoluteBounds();
 	paddedBounds.position += padding;
 	paddedBounds.size = max(glm::vec2(0.f), paddedBounds.size - padding * 2.f);
+	float viewLength = vertical ? paddedBounds.height : paddedBounds.width;
 
+	float freeLength = viewLength + spacing;
+	float relativeSizeTotal = 0.f;
+	for (const auto& child : getChildren()) {
+		if (!child->isActive() || !child->isVisible())
+			continue;
+
+		freeLength -= spacing;
+		if (vertical) {
+			if (child->layout.heightMode == SizingMode::Absolute)
+				freeLength -= child->layout.height;
+			else if (child->layout.heightMode == SizingMode::Relative)
+				relativeSizeTotal += child->layout.height;
+		} else {
+			if (child->layout.widthMode == SizingMode::Absolute)
+				freeLength -= child->layout.width;
+			else if (child->layout.widthMode == SizingMode::Relative)
+				relativeSizeTotal += child->layout.width;
+		}
+	}
+
+	freeLength = std::clamp(freeLength, 0.f, viewLength);
+	float lengthPerRelativeSize = relativeSizeTotal > 0.f ? freeLength / relativeSizeTotal : 0.f;
+
+	float currentPos = 0.f;
 	for (const auto& child : getChildren()) {
 		if (!child->isActive() || !child->isVisible())
 			continue;
 
 		child->layout.anchor = Anchor::TopLeft;
 
-		if (vertical)
-			child->layout.offset.y = currentPos - offset;
-		else
-			child->layout.offset.x = currentPos - offset;
-
-		child->updateBounds(paddedBounds);
+		Rectangle bounds;
+		bounds = paddedBounds;
+		if (vertical) {
+			if (child->layout.widthMode == SizingMode::Absolute)
+				child->layout.offset.y = currentPos - offset;
+			else if (child->layout.heightMode == SizingMode::Relative) {
+				bounds.y += currentPos - offset;
+				bounds.height = lengthPerRelativeSize;
+			}
+		} else {
+			if (child->layout.widthMode == SizingMode::Absolute)
+				child->layout.offset.x = currentPos - offset;
+			else if (child->layout.widthMode == SizingMode::Relative) {
+				bounds.x += currentPos - offset;
+				bounds.width = lengthPerRelativeSize;
+			}
+		}
+		child->updateBounds(bounds);
 
 		float childLength = vertical ? child->getAbsoluteBounds().height : child->getAbsoluteBounds().width;
 		currentPos += childLength + spacing;
@@ -28,7 +63,7 @@ void UIList::updateBounds(Rectangle parentBounds) {
 
 	totalContentLength = currentPos > 0.f ? currentPos - spacing : 0.f;
 
-	maxOffset = std::max(0.f, totalContentLength - (vertical ? paddedBounds.height : paddedBounds.width));
+	maxOffset = std::max(0.f, totalContentLength - viewLength);
 	offset = std::clamp(offset, 0.f, maxOffset);
 }
 
